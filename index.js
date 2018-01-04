8b87d6fd0d8f44dbefe13910f1abb4b6bbc922dd
Juul Rudihagen <juularthur92@gmail.com>
2018-01-04 10:56:34 +0100

    set port#

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var path = require('path');
 const app = express();
+app.set('port', (process.env.PORT || 3000));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(3001, () => console.log('CSRF attack is on!'));
+app.listen(process.env.PORT || 3001, () => console.log('CSRF attack is on!'));
